6b1b82e0bb02d9446ed1efca06ac8f4f5782c420
AhmedReda2024 <ahmedreda>
2025-06-04 23:49:57 +0300

    second_commit

--- src/app/core/services/branches/branches.service.ts
+++ src/app/core/services/branches/branches.service.ts
@@ -3,14 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BranchesService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor( private httpClient:HttpClient ) { }
-
-  getBranches():Observable<any>{
-    return this.httpClient.get('http://81.29.111.142:8085/CVM/CVMMobileAPIs/api/getBranches');
+  getBranches(): Observable<any> {
+    return this.httpClient.get(
+      'https://81.29.111.142:8085/CVM/CVMMobileAPIs/api/getBranches'
+    );
   }
-
 }

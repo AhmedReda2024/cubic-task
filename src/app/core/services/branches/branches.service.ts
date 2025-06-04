f08defab8bb9d88f6f3cb7599ed1631622947111
AhmedReda2024 <ahmedreda>
2025-06-05 00:46:02 +0300

    fourth_commit

--- src/app/core/services/branches/branches.service.ts
+++ src/app/core/services/branches/branches.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BranchesService {
   constructor(private httpClient: HttpClient) {}
+
+  private branchesUrl = `${environment.apiBaseUrl}/getBranches`;
 
   getBranches(): Observable<any> {
     return this.httpClient.get(

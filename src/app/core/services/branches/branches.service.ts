578e2bd1b30aa156f48cb6aeb327844cf297e5a3
AhmedReda2024 <ahmedreda>
2025-06-06 17:36:47 +0300

    last_commit

--- src/app/core/services/branches/branches.service.ts
+++ src/app/core/services/branches/branches.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,11 +8,9 @@ import { environment } from '../../../../environments/environment';
 export class BranchesService {
   constructor(private httpClient: HttpClient) {}
 
-  private branchesUrl = `${environment.apiBaseUrl}/getBranches`;
-
   getBranches(): Observable<any> {
     return this.httpClient.get(
-      'http://81.29.111.142:8085/CVM/CVMMobileAPIs/api/getBranches'
+      'https://cubic.sevencsolutions.com/api/getBranches'
     );
   }
 }
